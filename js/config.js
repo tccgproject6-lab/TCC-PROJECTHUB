de894cbb5e060398c0e5028103c9623c98dcc101
tccgproject6-lab <tccgproject6@gmail.com>
2026-08-15 12:15:58 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // js/config.js
 
-// Configuration mpya ya tccgproject6-lab
-export const SUPABASE_URL = 'https://exejmvckurtbdtcpjvxn.supabase.co';
-export const SUPABASE_ANON_KEY = 'EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4ZWptdmNrdXJ0YmR0Y3BqdnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3MjY0ODksImV4cCI6MjEwMjMwMjQ4OX0.nY1Xmb6gTi_MkVU8MSEd-tiz_hzuO8gPeOyapXGhsD8';
+// Configuration mpya ya tccgproject6-lab kwenye Window Scope
+window.SUPABASE_URL = 'https://exejmvckurtbdtcpjvxn.supabase.co';
+window.SUPABASE_KEY = 'EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4ZWptdmNrdXJ0YmR0Y3BqdnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3MjY0ODksImV4cCI6MjEwMjMwMjQ4OX0.nY1Xmb6gTi_MkVU8MSEd-tiz_hzuO8gPeOyapXGhsD8';
